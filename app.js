936e28041bf62afb6dc90889623bf40d44179325
Uenderss <meajudabarto@gmail.com>
2022-06-26 20:20:14 -0300

    implementação do POST participants com join

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import chalk from "chalk";
 import cors from "cors";
-import {MongoClient} from "mongodb";
+import dayjs from "dayjs";
+import joi from "joi";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 const app = express();
@@ -10,31 +12,64 @@ app.use(json());
 dotenv.config();
 
 //banco de dados
-const porta = process.env.PORTA||5000;
-let db=null;
+const porta = process.env.PORTA || 5000;
+let db = null;
 const urlBanco = process.env.URL_MONGO;
-const client= new MongoClient(urlBanco);
-const promise=client.connect();
-promise.then(()=>{
-  db=client.db(process.env.MEUBANCO);
-  console.log(chalk.green("Mongo conectado"));
-}).catch( e=> console.log("Falha na conexao do banco",e));
+const client = new MongoClient(urlBanco);
+const promise = client.connect();
+promise
+  .then(() => {
+    db = client.db(process.env.MEUBANCO);
+    console.log(chalk.green("Mongo conectado"));
+  })
+  .catch((e) => console.log("Falha na conexao do banco", e));
 
-app.post('/participants',async(req,res)=>{
-  const participants = req.body;
-  console.log(participants);
-
-})
-
-app.get('/participants',async(req,res)=>{
-  try{
-    const participants = await db.collection('participants').find().toArray();
-    res.send(participants);
-  }catch (e){
-    return res.status(500).send('erro',e);
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  const nameSchema = joi.object({ name: joi.string().required() });
+  const { erro } = nameSchema.validate(name);
+  if (erro) {
+    return res.sendStatus(422);
   }
-})
-app.listen(porta,()=>{
-    console.log(chalk.green.bold("servidor conectado a porta: "+porta));
+
+  try {
+    const lista = await db.collection("participants").findOne({ name });
+    if (lista) {
+      return res.sendStatus(409);
+    } else {
+      await db
+        .collection("participants")
+        .insertOne({ name, lastStatus: Date.now() });
+      await db
+        .collection("messages")
+        .insertOne({
+          from: name,
+          to: "Todos",
+          text: "entra na sala...",
+          type: "status",
+          time: dayjs().format("HH:mm:ss"),
+        });
+        res.sendStatus(201);
+    }
+  } catch (e) {
+    return console.log("Algo de errado não esta certo");
+  }
 });
 
+app.get("/participants", async (req, res) => {
+  try {
+    res.send( await db.collection("participants").find().toArray());
+  } catch (e) {
+    return res.status(500).send("erro", e);
+  }
+});
+app.get("/messages", async (req, res) => {
+  try {
+    res.send( await db.collection("messages").find().toArray());
+  } catch (e) {
+    return res.status(500).send("erro", e);
+  }
+});
+app.listen(porta, () => {
+  console.log(chalk.green.bold("servidor conectado a porta: " + porta));
+});
