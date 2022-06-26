e530916d4c511bad962cdf099090d73ea5693066
Uenderss <meajudabarto@gmail.com>
2022-06-26 09:39:41 -0300

    implementacao do get participants

--- app.js
+++ app.js
@@ -20,13 +20,29 @@ async function run(){
   try{
     await client.connect();
     db=client.db(process.env.MEUBANCO);
-    console.log("conectando mongodb")
+    console.log(chalk.green("Mongo conectado"));
+
   }catch{
     console.log(err);
   }
 }
 
 run();
+
+app.post('/participants',async(req,res)=>{
+  const participants = req.body;
+  console.log(participants);
+
+})
+
+app.get('/participants',async(req,res)=>{
+  try{
+    const participants = await db.collection('participants').find().toArray();
+    res.send(participants);
+  }catch (e){
+    return res.status(500).send('erro',e);
+  }
+})
 app.listen(porta,()=>{
     console.log(chalk.green.bold("servidor conectado a porta: "+porta));
 });
