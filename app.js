b11f44d86d5f8d4e7310960d6135b284dcdebd8d
Uenderss <meajudabarto@gmail.com>
2022-06-27 00:30:13 -0300

    implementação do post STATUS

--- app.js
+++ app.js
@@ -104,6 +104,21 @@ app.get("/messages", async (req, res) => {
   }
 });
 
+app.post("/status",async(req,res)=>{
+  const {user}=req.headers;
+  try {
+    const lista = await db.collection("participants").findOne({ name:user });
+    if (!lista) {
+      return res.sendStatus(404);
+    } else {
+      await db.collection("participants").updateOne({name:user},{$set:{lastStatus:Date.now()}})
+      return res.sendStatus(200);
+    }
+  }catch(erro){
+    res.status(500).send('Erro', erro)
+  }
+});
+
 app.listen(porta, () => {
   console.log(chalk.green.bold("servidor conectado a porta: " + porta));
 });
